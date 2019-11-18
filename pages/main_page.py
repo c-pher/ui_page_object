9888d1321157d84e5cae49f21ba1e37ffd77f431
agegemon <a.komisssarov@gmail.com>
2019-11-19 01:22:40 +0200

    locators added test refactored to use locators

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,12 +1,12 @@
-from selenium.webdriver.common.by import By
-
 from .base_page import BasePage
+from .locators import MainPageLocators
 
 
 class MainPage(BasePage):
+    link = 'http://selenium1py.pythonanywhere.com/'
+
     def go_to_login_page(self):
-        link = self.browser.find_element(By.ID, 'login_link')
-        link.click()
+        self.browser.find_element(*MainPageLocators.LOGIN_LINK).click()
 
     def should_be_login_link(self):
-        assert self.is_element_present(By.ID, 'login_link'), 'Login link is not presented'
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), 'Login link is not presented'
