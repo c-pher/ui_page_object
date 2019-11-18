6bf973bb1ec26290d2c136fb92e274ee8b7dcb3e
agegemon <a.komisssarov@gmail.com>
2019-11-19 00:39:24 +0200

    test refactored to use PO

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,7 @@
+from .pages.main_page import MainPage
+
 def test_guest_can_go_to_login_page(browser):
-    browser.get('http://selenium1py.pythonanywhere.com/')
-    go_to_login_page(browser)
-
-
-def go_to_login_page(browser):
-    link = browser.find_element_by_css_selector('#login_link')
-    link.click()
+    link = 'http://selenium1py.pythonanywhere.com/'
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
