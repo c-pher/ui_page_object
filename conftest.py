b58badc5aa1155f23be7b521b77f6b8d7c82f1be
agegemon <a.komisssarov@gmail.com>
2019-11-19 00:06:08 +0200

    Test ready to be used.

--- conftest.py
+++ conftest.py
@@ -4,19 +4,29 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default=None,
+    parser.addoption('--language', action='store', default='en',
                      help='Specify language param.')
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: chrome or firefox")
 
 
 @pytest.fixture
 def browser(request):
     user_language = request.config.getoption('language')
+    browser_name = request.config.getoption('browser_name')
 
-    options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    browser = webdriver.Chrome(options=options)
-    browser.implicitly_wait(5)
-
+    if browser_name == 'chrome':
+        print('\nStart chrome browser for test...')
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == 'firefox':
+        print('\nStart firefox browser for test...')
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference('intl.accept_languages', user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+    else:
+        raise pytest.UsageError('--browser_name should be chrome or firefox')
     yield browser
 
     browser.quit()
