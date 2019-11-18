92218092366e6ba06a86d7f4a29086a89de360ae
agegemon <a.komisssarov@gmail.com>
2019-11-18 22:37:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Final UI automation project
+
+
+Stepik: Selenium + Python + PageObject
+https://stepik.org/course/575/syllabus
---
+++ conftest.py
@@ -0,0 +1,22 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=None,
+                     help='Specify language param.')
+
+
+@pytest.fixture
+def browser(request):
+    user_language = request.config.getoption('language')
+
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
+    browser.implicitly_wait(5)
+
+    yield browser
+
+    browser.quit()
