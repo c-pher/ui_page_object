a9d59417170d411241644f463f900e6eb5f1e12f
agegemon <a.komisssarov@gmail.com>
2019-11-25 17:21:46 +0200

    test_guest_cant_see_product_in_basket_opened_from_main_page moved from test_main_page to the test_product_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,25 +1,7 @@
 import pytest
 
-from .pages.basket_page import BasketPage
 from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
-
-
-@pytest.mark.need_review
-def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-    """
-    Гость открывает главную страницу
-    Переходит в корзину по кнопке в шапке сайта
-    Ожидаем, что в корзине нет товаров
-    Ожидаем, что есть текст о том что корзина пуста
-    """
-
-    page = MainPage(browser, MainPage.link)
-    page.open()
-    page.go_to_basket_page()
-    basket_page = BasketPage(browser, BasketPage.link)
-    basket_page.should_be_empty()
-    basket_page.should_not_be_products()
 
 
 @pytest.mark.login_guest
--- test_product_page.py
+++ test_product_page.py
@@ -2,7 +2,9 @@ import time
 
 import pytest
 
+from .pages.basket_page import BasketPage
 from .pages.login_page import LoginPage
+from .pages.main_page import MainPage
 from .pages.product_page import ProductPage
 
 
@@ -69,7 +71,12 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     Ожидаем, что есть текст о том что корзина пуста
     """
 
-    pass
+    page = MainPage(browser, MainPage.link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, BasketPage.link)
+    basket_page.should_be_empty()
+    basket_page.should_not_be_products()
 
 
 class TestUserAddToBasketFromProductPage:
